7cb71ae0e3ca07ca89e9e6673bdea33a16253e3b
momiji <momiji>
2018-12-30 14:30:52 +0100

    Apollo Server and Client

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class LambdaDemo extends Component {
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: '/.netlify/functions/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+const LambdaDemo = () => (
+  <ApolloProvider client={client}>
+    <Query query={ gql`{ hello }` }>
+      { ({data}) => <div>A greeting from the server: {data && data.hello}</div> }
+    </Query>
+  </ApolloProvider>
+);
+
+class LambdaDemoX extends Component {
   constructor(props) {
     super(props);
     this.state = { loading: false, msg: null };
